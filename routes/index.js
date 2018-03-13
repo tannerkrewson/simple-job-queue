31b10ca3772778047281942ae94d27a1c819821e
tannerkrewson <tannerkrewson@gmail.com>
2018-03-12 22:22:19 -0500

    Added a basic UI and test add endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const { URL } = require('url');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Simple Job Queue', author: 'Tanner Krewson' });
+});
+
+router.post('/add', function(req, res, next) {
+    var theURL = req.body.url;
+
+    // add http to url if it's not there
+    if (!/^https?\:\/\//.test(theURL)) {
+        theURL = "http://" + theURL;
+    }
+
+    try {
+        theURL = new URL(theURL);
+        // do the thing
+        res.send('will do');
+    } catch (e) {
+        console.log(e);
+        res.status(400).send('Not a valid url: ' + req.body.url);
+    }
 });
 
 module.exports = router;
